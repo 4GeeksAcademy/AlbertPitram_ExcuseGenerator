b3881e36ab6a9f00f76f5615cddf5d86a93bcffc
AlbertPitram <albertvidallopez02@gmail.com>
2025-05-11 18:44:37 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+const who = ['The dog', 'Ryan Reynolds', 'Balatro Balatrez', 'Juan de dios'];
+const action = ['ate', 'gambled', 'sold', 'destroyed'];
+const what = ['my homework', 'my phone', 'the car', 'the PC'];
+const when = ['before the class', 'when I was playing Balatro', 'while I was gambling at the casino', 'while I was eating a Kebab', 'when I was learning how to speedrun Dark Souls'];
+
+
+ function randomExcuse() {
+      const randomWho = who[Math.floor(Math.random() * who.length)];
+      const randomAction = action[Math.floor(Math.random() * action.length)];
+      const randomWhat = what[Math.floor(Math.random() * what.length)];
+      const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+      return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+    }
+
+   document.getElementById("excuse").textContent = randomExcuse();
+   
 };
+
